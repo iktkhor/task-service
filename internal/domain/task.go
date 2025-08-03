849bd32d661fa71282af3526833fa1f108488cb6
Igor <txor2009@mail.ru>
2025-08-04 01:05:20 +0300

    fix task.go

--- internal/domain/task.go
+++ internal/domain/task.go
@@ -1,27 +1,31 @@
 package domain
 
-import "time"
-
-type TaskStatus string
-
-const (
-    StatusPending   TaskStatus = "pending"
-    StatusRunning   TaskStatus = "running"
-    StatusCompleted TaskStatus = "completed"
+import (
+	"net/http"
+	"time"
 )
 
 type Task struct {
     ID         string     `json:"id"`
-    Status     TaskStatus `json:"status"`
+    Status     int        `json:"status"`
     CreatedAt  time.Time  `json:"created_at"`
     StartedAt  time.Time  `json:"started_at,omitempty"`
     FinishedAt time.Time  `json:"finished_at,omitempty"`
-    Duration   string     `json:"duration,omitempty"`
 }
 
-type TaskResponse struct {
-    ID          string `json:"id"`
-    Status      string `json:"status"`
-    CreatedAt   string `json:"created_at"`
-    CurDuration string `json:"duration,omitempty"`
+func (t * Task) Duration() string {
+    var res string
+
+    switch t.Status {
+    case http.StatusCreated:
+        res = "Task not started yet"
+    case http.StatusProcessing:
+        res = time.Since(t.StartedAt).String()
+    case http.StatusOK:
+        res = t.FinishedAt.Sub(t.StartedAt).String()
+    default:
+        res = "Duration isn't avaible"
+    }
+
+    return res
 }
